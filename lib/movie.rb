41bfb492790999e5e048e6e594c0c7e8ad64c0c3
Elson Otake <elsonotake@yahoo.com.br>
2022-09-06 13:16:50 -0300

    Add attribute reader for variables

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,6 +1,8 @@
 require './item'
 
 class Movie < Item
+  attr_reader :source, :publish_date, :archived, :id, :silent
+
   # def initialize(genre, author, source, label, publish_date, silent)
   #   super(genre, author, source, label, publish_date)
   def initialize(source, publish_date, silent)
