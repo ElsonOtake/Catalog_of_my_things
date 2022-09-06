9d9d906045e736196948dbe09d2c88973828dca5
Elson Otake <elsonotake@yahoo.com.br>
2022-09-06 16:22:18 -0300

    Add tests for module Create

---
+++ lib/spec/create_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Create do
+  include Create
+  before :each do
+    @source_added = add_source('Newspaper')
+    @movie_added = add_movie(@source_added, 'Aug 30, 2022', false)
+  end
+
+  describe '#new' do
+    it 'source_added has the Source class' do
+      expect(@source_added).to be_instance_of Source
+    end
+
+    it 'source_added has the name Newspaper' do
+      expect(@source_added.name).to eq('Newspaper')
+    end
+
+    it 'movie_added has Movie class' do
+      expect(@movie_added).to be_instance_of Movie
+    end
+
+    it 'movie_added has silent as false' do
+      expect(@movie_added.silent).to be_falsey
+    end
+  end
+end
