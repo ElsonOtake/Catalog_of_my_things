b1f228ee33b905f0db392783947ece2d9dd4fbce
Elson Otake <elsonotake@yahoo.com.br>
2022-09-06 12:12:53 -0300

    Update unused class as comments

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,17 +1,17 @@
 require './item'
 
-# rubocop:disable Metrics/ParameterLists
 class Movie < Item
-  def initialize(genre, author, source, label, publish_date, silent)
-    super(genre, author, source, label, publish_date)
+  # def initialize(genre, author, source, label, publish_date, silent)
+  #   super(genre, author, source, label, publish_date)
+  def initialize(source, publish_date, silent)
+    super(source, publish_date)
     @silent = silent
   end
-  # rubocop:enable Metrics/ParameterLists
 
   private
 
   def can_be_archived?
-    return true if super || @silent
+    super || @silent
 
     false
   end
