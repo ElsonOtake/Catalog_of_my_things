c3845d4ac79a1836bec9331d350d35b996bff0ca
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-06 08:19:04 -0500

    fix linters

--- lib/books/book.rb
+++ lib/books/book.rb
@@ -2,7 +2,7 @@ require_relative '../item'
 
 class Book < Item
   def initialize(publisher, cover_state)
-    super(id, publidh_date, genre, author, source, label )
+    super(id, publidh_date, genre, author, source, label)
     @publisher = publisher
     @cover_state = cover_state
   end
