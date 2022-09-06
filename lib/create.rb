77e183423669b27f728fdee4b4b4a3e000134441
Elson Otake <elsonotake@yahoo.com.br>
2022-09-06 13:17:13 -0300

    Add add_movie method

--- lib/create.rb
+++ lib/create.rb
@@ -1,9 +1,16 @@
 require './source'
+require './movie'
 
 module Create
   def add_source(name)
     source = Source.new(name)
     puts "#{source.id} #{source.name}"
     source
+  end
+
+  def add_movie(source, publish_date, silent)
+    movie = Movie.new(source, publish_date, silent)
+    puts "#{source.id} #{source.name} #{movie.publish_date} #{movie.silent}"
+    movie
   end
 end
