21b70e967581cd84ac3ec54c1dcf64ca7fe5fc94
Elson Otake <elsonotake@yahoo.com.br>
2022-09-06 13:40:43 -0300

    Update comments removed

--- lib/create.rb
+++ lib/create.rb
@@ -3,14 +3,10 @@ require './movie'
 
 module Create
   def add_source(name)
-    source = Source.new(name)
-    puts "#{source.id} #{source.name}"
-    source
+    Source.new(name)
   end
 
   def add_movie(source, publish_date, silent)
-    movie = Movie.new(source, publish_date, silent)
-    puts "#{source.id} #{source.name} #{movie.publish_date} #{movie.silent}"
-    movie
+    Movie.new(source, publish_date, silent)
   end
 end
