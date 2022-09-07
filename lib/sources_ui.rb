c4af0c1e60748bba13dfa019f6165dd2c1823ab1
Elson Otake <elsonotake@yahoo.com.br>
2022-09-07 00:14:08 -0300

    Add sources ui file

---
+++ lib/sources_ui.rb
@@ -0,0 +1,11 @@
+class UiSources
+
+  def initialize(app)
+    @app = app
+  end
+
+  def list_all_sources
+    puts 'The sources list is empty!' if @app.list_sources.length.zero?
+    @app.list_sources.each { |source| puts "Name: \"#{source.name}\"" }
+  end
+end
