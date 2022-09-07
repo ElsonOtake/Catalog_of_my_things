7a7a76023ab9fdf8b932403855d321cbb85bc8c5
Andyveloper <mcantillo.gdi@gmail.com>
2022-09-07 16:32:51 -0500

    Adjust unit tests, all tests passing

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -1,17 +1,35 @@
-require './item'
+require_relative './item'
+
+
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify, :genre, :publish_date
 
-  def initialize(on_spotify = 'true')
+  attr_accessor :on_spotify, :publish_date, :genre, :source
+
+
+
+  def initialize(on_spotify, publish_date)
+
     super(publish_date)
 
+
+
     @on_spotify = on_spotify
+
   end
+
+
 
   private
 
+
+
   def can_be_archived?
+
     can_be_archived? || @on_spotify
+
   end
+
 end
+
+
