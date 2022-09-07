8aa1c1a27574c2fe0680e7c9e86e742e17d534db
Elson Otake <elsonotake@yahoo.com.br>
2022-09-06 22:01:57 -0300

    Remove properties from contructor method

--- lib/item.rb
+++ lib/item.rb
@@ -1,15 +1,32 @@
 require 'date'
 
 class Item
-  def initialize(genre, author, source, label, publish_date)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
+  attr_reader :genre, :author, :source, :label
+
+  def initialize(publish_date)
     @publish_date = Date.parse(publish_date)
     @archived = false
     @id = Random.rand(1..10_000)
-    @source.add_item(self)
+  end
+
+  def genre=(genre)
+    @genre = genre unless @genre.eql?(genre)
+    @genre.add_item(self) unless @genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author unless @author.eql?(author)
+    @author.add_item(self) unless @author.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source unless @source.eql?(source)
+    @source.add_item(self) unless @source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label unless @label.eql?(label)
+    @label.add_item(self) unless @label.items.include?(self)
   end
 
   def move_to_archive
