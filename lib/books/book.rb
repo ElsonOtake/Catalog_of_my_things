d629edd3fd1500dae60ed253ef78934bdb3552ec
Elson Otake <elsonotake@yahoo.com.br>
2022-09-07 23:00:06 -0300

    Fix RSpec error

--- lib/books/book.rb
+++ lib/books/book.rb
@@ -4,8 +4,8 @@ class Book < Item
   attr_accessor :publisher, :cover_state, :publish_date
   attr_reader :id
 
-  def initialize(publisher, cover_state, publish_date)
-    super(publish_date)
+  def initialize(title, publisher, cover_state, publish_date)
+    super(title, publish_date)
     @id = Random.rand(1..1000)
     @publisher = publisher
     @cover_state = cover_state
