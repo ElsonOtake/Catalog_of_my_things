96a2cedd7c47c69b999b743197083be01d7498ed
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-08 08:08:14 -0500

    reverse main.rb mistaken commits

--- lib/main.rb
+++ lib/main.rb
@@ -25,11 +25,11 @@ class Main
   end
 
   def menu
-    methods = { '1' => 'list_all_books', '2' => 'list_all_movies',
+    methods = { '1' => 'list_all_movies', '2' => 'list_all_movies',
                 '3' => 'list_all_movies', '4' => 'list_all_movies',
-                '5' => 'list_all_sources', '6' => 'list_all_labels',
+                '5' => 'list_all_sources', '6' => 'list_all_sources',
                 '7' => 'list_all_sources', '8' => 'list_all_sources',
-                '9' => 'add_a_book', '10' => 'add_a_movie', '11' => 'add_a_movie', '12' => 'add_a_movie' }
+                '9' => 'add_a_movie', '10' => 'add_a_movie', '11' => 'add_a_movie', '12' => 'add_a_movie' }
 
     loop do
       case @option = menu_option
