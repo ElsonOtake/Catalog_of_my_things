d2e6deb4a22cb44974c672e764d15bdb759ad723
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-07 23:20:27 -0500

    Add ui methods

--- lib/app.rb
+++ lib/app.rb
@@ -1,14 +1,16 @@
 require_relative './movie'
-
 require_relative './source'
+require_relative './books/book'
+require_relative './label'
 
 class App
-  attr_writer :list_of_movies, :list_of_sources
+  attr_writer :list_of_movies, :list_of_sources, :list_of_books, :list_all_labels
 
   def initialize
     @list_of_movies = []
-
     @list_of_sources = []
+    @list_of_books = []
+    @list_of_labels = []
   end
 
   def list_movies
@@ -19,29 +21,45 @@ class App
     @list_of_sources
   end
 
+  def list_books
+    @list_of_books
+  end
+
+  def list_labels
+    @list_of_labels
+  end
+
   def add_movie(publish_date, silent, *param)
     _genre, _author, source, _label = *param
-
     movie = Movie.new(publish_date, silent)
-
     movie.genre = source
-
     movie.author = source
-
     movie.source = source
-
     movie.label = source
-
     @list_of_movies << movie
-
     movie
   end
 
   def add_source(name)
     source = Source.new(name)
-
     @list_of_sources << source
-
     source
+  end
+
+  def add_book(publisher, cover_state, publish_date, *param)
+    genre, author, source, label = *param
+    book = Book.new(publisher, cover_state, publish_date)
+    book.genre = genre
+    book.author = author
+    book.source = source
+    book.label = label
+    @list_of_books << book
+    book
+  end
+
+  def add_label(title, color)
+    label = Label.new(title, color)
+    @list_of_labels << label
+    label
   end
 end
