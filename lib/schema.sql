e611ae27fabb8710083d8e91141e0a57f30fbb7a
Elson Otake <elsonotake@yahoo.com.br>
2022-09-08 16:41:38 -0300

    Add foreign keys for tables

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,28 +1,30 @@
 CREATE DATABASE catalog_of_my_things;
 
 CREATE TABLE item (
-  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id integer GENERATED ALWAYS AS IDENTITY,
   title varchar(100) NOT NULL,
   genre_id integer NOT NULL,
   author_id integer NOT NULL,
   source_id integer NOT NULL,
   label_id integer NOT NULL,
   publish_date date NOT NULL,
-  archived boolean
+  archived boolean,
+  PRIMARY KEY(id),
+   CONSTRAINT fk_source
+      FOREIGN KEY(source_id) 
+	  REFERENCES source(id)
 );
-
-CREATE INDEX ON item (genre_id);
-
-CREATE INDEX ON item (author_id);
 
 CREATE INDEX ON item (source_id);
 
-CREATE INDEX ON item (label_id);
-
 CREATE TABLE movie (
-  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id integer GENERATED ALWAYS AS IDENTITY,
   silent boolean NOT NULL,
-  item_id integer NOT NULL
+  item_id integer NOT NULL,
+  PRIMARY KEY(id),
+   CONSTRAINT fk_item
+      FOREIGN KEY(item_id) 
+	  REFERENCES item(id)
 );
 
 CREATE INDEX ON movie (item_id);
