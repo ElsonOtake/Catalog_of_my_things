f21285026a19595e13e22c22fae902aef50996ca
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-07 23:27:22 -0500

    add other complementary methods

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,8 @@ require_relative './movie'
 require_relative './source'
 require_relative './books/book'
 require_relative './label'
+require_relative './genre'
+require_relative './music_album'
 
 class App
   attr_writer :list_of_movies, :list_of_sources, :list_of_books, :list_all_labels
@@ -11,6 +13,8 @@ class App
     @list_of_sources = []
     @list_of_books = []
     @list_of_labels = []
+    @list_of_music_albums = []
+    @list_of_genres = []
   end
 
   def list_movies
@@ -27,6 +31,14 @@ class App
 
   def list_labels
     @list_of_labels
+  end
+
+  def list_music_albums
+    @list_of_music_albums
+  end
+
+  def list_genres
+    @list_of_genres
   end
 
   def add_movie(publish_date, silent, *param)
@@ -57,9 +69,26 @@ class App
     book
   end
 
+  def add_music_album(publish_date, on_spotify, *param)
+    genre, _author, source, _label = *param
+    music_album = MusicAlbum.new(publish_date, on_spotify)
+    music_album.genre = genre
+    music_album.author = genre
+    music_album.source = source
+    music_album.label = genre
+    @list_of_music_albums << music_album
+    music_album
+  end
+
   def add_label(title, color)
     label = Label.new(title, color)
     @list_of_labels << label
     label
+  end
+
+  def add_genre(name)
+    genre = Genre.new(name)
+    @list_of_genres << genre
+    genre
   end
 end
