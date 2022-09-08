6a7bf7f598761f7ade9b5d909cde97510fb97328
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-08 16:28:05 -0500

    Add create label method

--- lib/main.rb
+++ lib/main.rb
@@ -11,6 +11,8 @@ class Main
     @app = App.new
     @ui = Ui.new(@app)
     @option = ''
+    @app.list_of_sources = reader_source
+    @app.list_of_movies = reader_movie
   end
 
   def menu_content
@@ -26,37 +28,35 @@ class Main
     @option = check_input('') { %w[0 1 2 3 4 5 6 7 8 9 10 11 12].include?(@option) }
   end
 
-  def menu
-    methods = { '1' => 'list_all_books', '2' => 'list_all_movies',
-                '3' => 'list_all_movies', '4' => 'list_all_movies',
-                '5' => 'list_all_sources', '6' => 'list_all_labels',
-                '7' => 'list_all_sources', '8' => 'list_all_sources',
-                '9' => 'add_a_book', '10' => 'add_a_movie', '11' => 'add_a_movie', '12' => 'add_a_movie' }
+  def create_label
+    @app.create_label('Gift', 'blue')
+    @app.create_label('New', 'red')
+    @app.create_label('recently earned', 'brown')
+    @app.create_label('Old', 'green')
+    @app.create_label('From a friend', 'black')
+  end
 
+  def menu
+    methods = { '1' => 'list_all_movies', '2' => 'list_all_movies',
+                '3' => 'list_all_movies', '4' => 'list_all_movies',
+                '5' => 'list_all_sources', '6' => 'list_all_sources',
+                '7' => 'list_all_sources', '8' => 'list_all_sources',
+                '9' => 'add_a_movie', '10' => 'add_a_movie',
+                '11' => 'add_a_movie', '12' => 'add_a_movie' }
     loop do
       case @option = menu_option
       when '0'
+        writer
         puts "Thank you for using this app!\n"
-        book_label_writer
         break
       else
         @ui.send(methods[@option])
       end
     end
   end
-
-  def create_sources
-    @app.add_source('From a friend')
-    @app.add_source('Online shop')
-    @app.add_source('Amazon Prime Video')
-    @app.add_source('Google Play')
-    @app.add_source('Vudu')
-    @app.add_source('Apple iTunes')
-    @app.add_source('YouTube')
-  end
 end
 
 puts "\nWelcome to Catalog of my things App!"
 main = Main.new
-main.create_sources
+main.create_label
 main.menu
