0c9b6a18cb0e5449ef50d05673cf2dab6183ae69
Elson Otake <elsonotake@yahoo.com.br>
2022-09-07 21:34:03 -0300

    Update user interface for other classes

--- lib/ui.rb
+++ lib/ui.rb
@@ -31,6 +31,32 @@ class Ui
     @app.list_sources.each { |source| puts "  #{source.name}" }
   end
 
+  def select_genre
+    puts 'Select a genre from the following list by number'
+    @app.list_genres.each_with_index do |genre, index|
+      puts "#{index}) \"#{genre.name}\""
+    end
+  end
+
+  def list_genres_option
+    select_genre
+    list_size = @app.list_genres.length
+    @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
+  end
+
+  def select_author
+    puts 'Select an author from the following list by number'
+    @app.list_authors.each_with_index do |author, index|
+      puts "#{index}) \"#{author.name}\""
+    end
+  end
+
+  def list_authors_option
+    select_author
+    list_size = @app.list_authors.length
+    @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
+  end
+
   def select_source
     puts 'Select a source from the following list by number'
     @app.list_sources.each_with_index do |source, index|
@@ -44,20 +70,43 @@ class Ui
     @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
   end
 
-  def add_a_movie
+  def select_label
+    puts 'Select a label from the following list by number'
+    @app.list_labels.each_with_index do |label, index|
+      puts "#{index}) \"#{label.name}\""
+    end
+  end
+
+  def list_label_option
+    select_label
+    list_size = @app.list_labels.length
+    @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
+  end
+
+  def check_empty
+    puts 'The genre list is empty!' if @app.list_genres.length.zero?
+    puts 'The author list is empty!' if @app.list_authors.length.zero?
     puts 'The source list is empty!' if @app.list_sources.length.zero?
-    return if @app.list_sources.length.zero?
+    puts 'The label list is empty!' if @app.list_labels.length.zero?
+  end
+
+  def add_a_movie
+    check_empty
+    return if @app.list_genres.length.zero? || @app.list_authors.length.zero? || 
+              @app.list_sources.length.zero? || @app.list_labels.length.zero?
 
     title = check_input('Title: ') { @option != '' }
-    genre = ''
-    author = ''
+    genre = list_genre_option
+    author = list_author_option
     source = list_source_option
-    label = ''
+    label = list_label_option
     publish_date = check_input('Publish date: YYYY/MM/DD ') do
       @option.match?(%r{^(19|20)\d\d/(0[1-9]|1[012])/(0[1-9]|[1-2][0-9]|3[0-1])$})
     end
     silent = check_input('Silent: [true/false] ') { %w[true false].include?(@option.downcase) }
-    @app.add_movie(title, publish_date, silent, genre, author, @app.list_sources[source.to_i], label)
+    @app.add_movie(title, publish_date, silent, @app.list_genres[genre.to_i],
+                  @app.list_authors[author.to_i], @app.list_sources[source.to_i],
+                  @app.list_labels[label.to_i])
     puts 'Movie created successfully'
   end
 end
