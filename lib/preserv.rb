1b7e12a4c64cfe33e2c183f0293dfd5ae71346d0
Elson Otake <elsonotake@yahoo.com.br>
2022-09-07 21:40:19 -0300

    Update other classes data

--- lib/preserv.rb
+++ lib/preserv.rb
@@ -7,9 +7,9 @@ module Preserv
     File.write('data/source.json', '', mode: 'w')
 
     @app.list_movies.each do |movie|
-      array_movie = [movie.title, movie.publish_date, movie.silent, movie.genre.name, 
-                    movie.author.first_name, movie.author.last_name, movie.source.name,
-                    movie.label.title, movie.label.color ]
+      array_movie = [movie.title, movie.publish_date, movie.silent, movie.genre.name,
+                     movie.author.first_name, movie.author.last_name, movie.source.name,
+                     movie.label.title, movie.label.color]
       json_movie = JSON.generate(array_movie)
       File.write('data/movie.json', "#{json_movie}\n", mode: 'a')
     end
@@ -19,23 +19,25 @@ module Preserv
     end
   end
 
+  def read_movie_file
+    movies = []
+    File.foreach('data/movie.json') do |line|
+      movie = JSON.parse(line)
+      genre = @app.list_genres.select { |gnr| gnr.name == movie[3] }
+      author = @app.list_authors.select { |aut| aut.first_name == movie[4] && aut.last_name == movie[5] }
+      source = @app.list_sources.select { |src| src.name == movie[6] }
+      label = @app.list_labels.select { |lbl| lbl.title == movie[7] && lbl.color == movie[8] }
+      movies << @app.add_movie(movie[0], movie[1], movie[2], genre[0], author[0], source[0], label[0])
+    end
+    movies
+  end
+
   def reader_movie
     unless File.exist?('data/movie.json')
       File.open('data/movie.json', 'w')
       return []
     end
-    movies = []
-    File.foreach('data/movie.json') do |line|
-      movie = JSON.parse(line)
-      # genre = @app.list_genres.select { |gnr| gnr.name == movie[3] }
-      # author = @app.list_authors.select { |aut| aut.first_name == movie[4] && aut.last_name == movie[5] }
-      source = @app.list_sources.select { |src| src.name == movie[6] }
-      # label = @app.list_labels.select { |lbl| lbl.title == movie[7] && lbl.color == movie[8] }
-      movies << @app.add_movie(movie[0], movie[1], movie[2],
-                               source[0], source[0], source[0], source[0])
-                              #  genre[0], author[0], source[0], label[0])
-    end
-    movies
+    read_movie_file
   end
 
   def reader_source
