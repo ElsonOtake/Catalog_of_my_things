a3eca80c8d421d5ab3de281bbb018df8b002923a
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-09 00:15:22 -0500

    refactor games

--- lib/games/games.rb
+++ lib/games/games.rb
@@ -1,11 +1,11 @@
 require_relative '../item'
 
 class Games < Item
+  attr_accessor :title, :publish_date, :multiplayer, :last_played_a, :id
+
   def initialize(title, publish_date, multiplayer, last_played_at)
     super(title, publish_date)
-
     @multiplayer = multiplayer
-
     @last_played_at = last_played_at
   end
 
@@ -17,7 +17,3 @@ class Games < Item
     super && dif_time > 2
   end
 end
-
-# games = Games.new('2000-02-02', true, '2010-01-01')
-
-# p games
