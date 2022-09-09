f4aff6105b4f3ab290c27c824d635fe41739bb3b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-09 00:14:58 -0500

    Add methods to ui

--- lib/ui.rb
+++ lib/ui.rb
@@ -24,10 +24,60 @@ class Ui
     end
   end
 
+  # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  def list_all_books
+    puts "Books list\n\n"
+    puts 'The book list is empty!' if @app.list_books.length.zero?
+    @app.list_books.each do |book|
+      puts "  Title: #{book.title}"
+      puts "  Publish date: #{book.publish_date}"
+      puts "  Genre: #{book.source.name}" ## missing genre change
+      puts "  Author: #{book.source.name} #{book.source.name}" # pte author
+      # #{book.author.first_name} #{book.author.last_name}
+      puts "  Source: #{book.source.name}"
+      puts "  Label title: #{book.label.title} label color: #{book.label.color}"
+      puts "  Cover state: #{book.cover_state}"
+      puts " Id: #{book.id}"
+      puts
+    end
+  end
+  # NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDD
+
+  def list_all_music_albums
+    puts "Music list\n\n"
+    puts 'The music album list is empty!' if @app.list_music_albums.length.zero?
+    @app.list_music_albums.each do |music_album|
+      puts "  Title: #{music_album.title}"
+      puts "  Publish date: #{music_album.publish_date}"
+      puts "  Genre: #{music_album.genre.name}"
+      puts "  Author: #{music_album.genre.name}"
+      puts "  Source: #{music_album.source.name}"
+      puts "  Label title: #{music_album.label.title} label color: #{music_album.label.color}"
+      puts "  On Spotify: #{music_album.on_spotify}"
+      puts "  Archived: #{music_album.archived}"
+      puts "  Id: #{music_album.id}"
+      puts
+    end
+  end
+
   def list_all_sources
     puts "Source list\n\n"
     puts 'The sources list is empty!' if @app.list_sources.length.zero?
     @app.list_sources.each { |source| puts "  #{source.name}" }
+  end
+
+  # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  def list_all_labels
+    puts "Label list\n\n"
+    puts 'The label list is empty!' if @app.list_labels.length.zero?
+    @app.list_labels.each { |label| puts "  #{label.title}" }
+  end
+  # NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDD
+
+  def list_all_genres
+    puts "Genre list\n\n"
+    puts 'The Genre list is empty!' if @app.list_genres.length.zero?
+    @app.list_genres.each { |genre| puts "  #{genre.name}" }
   end
 
   def select_source
@@ -37,9 +87,40 @@ class Ui
     end
   end
 
+  # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  def select_label
+    puts 'Select a label from the following list by number'
+    @app.list_labels.each_with_index do |label, index|
+      puts "#{index}) \"#{label.title}\""
+    end
+  end
+  # NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDD
+
+  def select_genre
+    puts 'Select a genre from the following list by number'
+    @app.list_genres.each_with_index do |genre, index|
+      puts "#{index}) Genre: \"#{genre.name}\""
+    end
+  end
+
   def list_source_option
     select_source
     list_size = @app.list_sources.length
+    @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
+  end
+
+  # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  def list_label_option
+    select_label
+    list_size = @app.list_labels.length
+    @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
+  end
+  # NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDD
+
+  def list_genre_option
+    select_genre
+    list_size = @app.list_genres.length
+
     @option = check_input('') { @option.match?(/^\d+$/) && (0..list_size - 1).any? { |a| a == @option.to_i } }
   end
 
@@ -58,5 +139,43 @@ class Ui
     silent = check_input('Silent: [true/false] ') { %w[true false].include?(@option.downcase) }
     @app.add_movie(title, publish_date, silent, genre, author, @app.list_sources[source.to_i], label)
     puts 'Movie created successfully'
+  end
+
+  # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  def add_a_book
+    title = check_input('Title: ') { @option != '' }
+    genre = ''
+    author = ''
+    source = list_source_option
+    label = list_label_option
+    publish_date = check_input('Publish date: YYYY/MM/DD ') do
+      @option.match?(%r{^(19|20)\d\d/(0[1-9]|1[012])/(0[1-9]|[1-2][0-9]|3[0-1])$})
+    end
+    puts 'please enter publisher name'
+    publisher = gets.chomp
+    puts 'please enter the cover state [good/bad]'
+    cover_state = gets.chomp
+    @app.add_book(title, publisher, cover_state, publish_date, genre, author, @app.list_sources[source.to_i],
+                  @app.list_labels[label.to_i])
+    puts 'Book created successfully'
+  end
+  # NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDD
+
+  def add_a_music_album
+    puts 'The list is empty!' if @app.list_sources.length.zero?
+    return if @app.list_sources.length.zero?
+
+    title = check_input('Title: ') { @option != '' }
+    genre = list_genre_option
+    author = ''
+    source = list_source_option
+    label = list_label_option
+    publish_date = check_input('Publish date: YYYY/MM/DD ') do
+      @option.match?(%r{^(19|20)\d\d/(0[1-9]|1[012])/(0[1-9]|[1-2][0-9]|3[0-1])$})
+    end
+    on_spotify = check_input('Is it on Spotify: [true/false] ') { %w[true false].include?(@option.downcase) }
+    @app.add_music_album(title, publish_date, on_spotify, @app.list_genres[genre.to_i],
+                         @app.list_sources[source.to_i], author, @app.list_labels[label.to_i])
+    puts 'Music Album created successfully'
   end
 end
