d2feabd2a21bcd3c31bb0965f7bceb28ab1b6703
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-09 00:14:24 -0500

    refactor music_album

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -1,9 +1,9 @@
 require_relative './item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify, :publish_date, :genre, :source
+  attr_accessor :on_spotify, :publish_date, :archived, :id, :genre, :source, :title, :label, :author
 
-  def initialize(title, on_spotify, publish_date)
+  def initialize(title, publish_date, on_spotify)
     super(title, publish_date)
     @on_spotify = on_spotify
   end
@@ -11,7 +11,6 @@ class MusicAlbum < Item
   private
 
   def can_be_archived?
-    can_be_archived? || @on_spotify
+    super || @on_spotify
   end
 end
-# #this is the copy
