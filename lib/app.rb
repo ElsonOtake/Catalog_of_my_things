aadb24d332ecdf878eb34a15f52a3a3060030fe6
Elson Otake <elsonotake@yahoo.com.br>
2022-09-10 15:47:06 -0300

    Update order of methods according to UML

--- lib/app.rb
+++ lib/app.rb
@@ -1,101 +1,71 @@
-require_relative './movie'
-require_relative './source'
-require_relative './books/book'
-require_relative './label'
 require_relative './genre'
-require_relative './music_album'
 require_relative './author'
+require_relative './source'
+require_relative './label'
+require_relative './books/book'
+require_relative './music_album'
+require_relative './movie'
 require_relative './games/games'
 
 class App
-  attr_writer :list_of_movies, :list_of_sources, :list_of_books, :list_of_labels, :list_of_music_albums,
-              :list_of_genres, :list_of_games, :list_of_authors
+  attr_writer :list_of_genres, :list_of_authors, :list_of_sources, :list_of_labels,
+  :list_of_books, :list_of_music_albums, :list_of_movies, :list_of_games
+              
 
   def initialize
-    @list_of_movies = []
-    @list_of_sources = []
-    @list_of_books = []
-    @list_of_labels = []
-    @list_of_music_albums = []
     @list_of_genres = []
-    @list_of_games = []
     @list_of_authors = []
+    @list_of_sources = []
+    @list_of_labels = []
+    @list_of_books = []
+    @list_of_music_albums = []
+    @list_of_movies = []
+    @list_of_games = []
   end
 
-  def list_movies
-    @list_of_movies
-  end
-
-  def list_sources
-    @list_of_sources
-  end
-
-  def list_books
-    @list_of_books
-  end
-
-  def list_labels
-    @list_of_labels
-  end
-
-  def list_music_albums
-    @list_of_music_albums
-  end
 
   def list_genres
     @list_of_genres
-  end
-
-  def list_games
-    @list_of_games
   end
 
   def list_authors
     @list_of_authors
   end
 
-  def add_movie(title, publish_date, silent, *param)
-    genre, author, source, label = *param
-    movie = Movie.new(title, publish_date, silent)
-    movie.genre = genre
-    movie.author = author
-    movie.source = source
-    movie.label = label
-    @list_of_movies << movie
-    movie
+  def list_sources
+    @list_of_sources
   end
 
-  def add_book(title, publisher, cover_state, publish_date, *param)
-    _genre, _author, source, label = *param
-    book = Book.new(title, publisher, cover_state, publish_date)
-    book.genre = source
-    book.author = source
-    book.source = source
-    book.label = label
-    @list_of_books << book
-    book
+  def list_labels
+    @list_of_labels
   end
 
-  def add_music_album(title, publish_date, on_spotify, *param)
-    genre, source, _author, label = *param
-    music_album = MusicAlbum.new(title, publish_date, on_spotify)
-    music_album.genre = genre
-    music_album.source = source
-    music_album.author = source
-    music_album.label = label
-    @list_of_music_albums << music_album
-    music_album
+  def list_books
+    @list_of_books
   end
 
-  def add_game(title, publish_date, multiplayer, last_played_at, *param)
-    genre, author, source, label = *param
-    game = Games.new(title, publish_date, multiplayer, last_played_at)
-    game.genre = genre
-    game.author = author
-    game.source = source
-    game.label = label
-    @list_of_games << game
-    game
+  def list_music_albums
+    @list_of_music_albums
+  end
+
+  def list_movies
+    @list_of_movies
+  end
+
+  def list_games
+    @list_of_games
+  end
+
+  def add_genre(name)
+    genre = Genre.new(name)
+    @list_of_genres << genre
+    genre
+  end
+
+  def add_author(first_name, last_name)
+    author = Author.new(first_name, last_name)
+    @list_of_authors << author
+    author
   end
 
   def add_source(name)
@@ -110,15 +80,47 @@ class App
     label
   end
 
-  def add_genre(name)
-    genre = Genre.new(name)
-    @list_of_genres << genre
-    genre
+  def add_book(title, publisher, cover_state, publish_date, *param)
+    genre, author, source, label = *param
+    book = Book.new(title, publisher, cover_state, publish_date)
+    book.genre = genre
+    book.author = author
+    book.source = source
+    book.label = label
+    @list_of_books << book
+    book
   end
 
-  def add_author(first_name, last_name)
-    author = Author.new(first_name, last_name)
-    @list_of_authors << author
-    author
+  def add_music_album(title, publish_date, on_spotify, *param)
+    genre, source, _author, label = *param
+    music_album = MusicAlbum.new(title, publish_date, on_spotify)
+    music_album.genre = genre
+    music_album.author = author
+    music_album.source = source
+    music_album.label = label
+    @list_of_music_albums << music_album
+    music_album
+  end
+
+  def add_movie(title, publish_date, silent, *param)
+    genre, author, source, label = *param
+    movie = Movie.new(title, publish_date, silent)
+    movie.genre = genre
+    movie.author = author
+    movie.source = source
+    movie.label = label
+    @list_of_movies << movie
+    movie
+  end
+
+  def add_game(title, publish_date, multiplayer, last_played_at, *param)
+    genre, author, source, label = *param
+    game = Games.new(title, publish_date, multiplayer, last_played_at)
+    game.genre = genre
+    game.author = author
+    game.source = source
+    game.label = label
+    @list_of_games << game
+    game
   end
 end
