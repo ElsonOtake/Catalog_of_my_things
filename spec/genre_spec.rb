6655393d0a9af93ddcdc01443b3ccc8c73f4d9e3
Elson Otake <elsonotake@yahoo.com.br>
2026-09-11 16:12:31 -0300

    add genre tests

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -6,18 +6,37 @@ describe Genre do
       @new_genre = Genre.new('Rock')
     end
 
-    context 'with valid input' do
-      it 'should return a correct name' do
-        expect(@new_genre.name).to eq('Rock')
+    it 'should return a correct name' do
+      expect(@new_genre.name).to eq('Rock')
 
-        expect(@new_genre.name == 'Rock').to be_truthy
-      end
+      expect(@new_genre.name == 'Rock').to be_truthy
+    end
 
-      it 'should return a correct name' do
-        expect(@new_genre.name).not_to eq('Bass')
+    it 'should return a correct name' do
+      expect(@new_genre.name).not_to eq('Bass')
 
-        expect(@new_genre.name == 'Bass').to be_falsy
-      end
+      expect(@new_genre.name == 'Bass').to be_falsy
+    end
+
+    it 'should have a numeric id between 1 and 1000' do
+      id = @new_genre.instance_variable_get(:@id)
+      expect(id).to be_between(1, 1000).inclusive
+    end
+
+    it 'should add an item to itself and link back to the item' do
+      item = Item.new('Thing', 'May 15, 2020')
+      @new_genre.add_item(item)
+
+      expect(@new_genre.items).to include(item)
+      expect(item.genre).to eq(@new_genre)
+    end
+
+    it 'should not add the same item twice' do
+      item = Item.new('Thing', 'May 15, 2020')
+      @new_genre.add_item(item)
+      @new_genre.add_item(item)
+
+      expect(@new_genre.items.count(item)).to eq(1)
     end
   end
 end
